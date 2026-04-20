dd9eec689d3ae4258d4ea93fd289a60cf9743196
Daniel St <webdeconline@gmail.com>
2026-04-20 14:44:33 +0200

    update

---
+++ Webapp/RaceCore/StageProject_RaceCore/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using StageProject_RaceCore.Models;
+using System.Diagnostics;
+
+namespace StageProject_RaceCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
