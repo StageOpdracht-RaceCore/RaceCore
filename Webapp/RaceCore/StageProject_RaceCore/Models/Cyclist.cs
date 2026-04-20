b6bf27f19b0c16e780b78110e173024935faf278
Jason Peleman <jason.peleman@student.ucll.be>
2026-04-20 15:42:33 +0200

    add EF Core setup and ignore generated files

---
+++ Webapp/RaceCore/StageProject_RaceCore/Models/Cyclist.cs
@@ -0,0 +1,11 @@
+﻿namespace StageProject_RaceCore.Models
+{
+    public class Cyclist
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public long TeamId { get; set; }
+        public bool IsActive { get; set; } = true;
+    }
+}
--- Webapp/RaceCore/StageProject_RaceCore/Program.cs
+++ Webapp/RaceCore/StageProject_RaceCore/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace StageProject_RaceCore
 {
     public class Program
@@ -8,6 +10,9 @@ namespace StageProject_RaceCore
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<Models.AppDbContext>(options =>
+                options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 
