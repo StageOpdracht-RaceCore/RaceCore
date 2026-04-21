82f356ab55f57435ddabf50f5f930d525626e32b
Daniel St <webdeconline@gmail.com>
2026-04-21 11:55:49 +0200

    add controllers

---
+++ Webapp/RaceCore/StageProject_RaceCore/Controllers/PlayerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StageProject_RaceCore.Controllers
+{
+    public class PlayerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
