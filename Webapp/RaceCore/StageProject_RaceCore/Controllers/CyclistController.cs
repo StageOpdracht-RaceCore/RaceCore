6cb838a005132e805d71757cadf56f9000d5d8ad
Jason Peleman <jason.peleman@student.ucll.be>
2026-04-21 11:47:19 +0200

    cyclist view

--- Webapp/RaceCore/StageProject_RaceCore/Controllers/CyclistController.cs
+++ Webapp/RaceCore/StageProject_RaceCore/Controllers/CyclistController.cs
@@ -15,6 +15,81 @@ namespace StageProject_RaceCore.Controllers
 
         public async Task<IActionResult> Index(string? searchString, string? activeFilter)
         {
+            var cyclists = new List<Cyclist>
+            {
+                new Cyclist
+                {
+                    Id = 1,
+                    FirstName = "Wout",
+                    LastName = "Van Aert",
+                    IsActive = true,
+                    Team = new Team { Name = "Visma | Lease a Bike" }
+                },
+                new Cyclist
+                {
+                    Id = 2,
+                    FirstName = "Tadej",
+                    LastName = "Pogacar",
+                    IsActive = true,
+                    Team = new Team { Name = "UAE Team Emirates" }
+                },
+                new Cyclist
+                {
+                    Id = 3,
+                    FirstName = "Tom",
+                    LastName = "Dumoulin",
+                    IsActive = false,
+                    Team = new Team { Name = "Retired" }
+                },
+                new Cyclist
+                {
+                    Id = 4,
+                    FirstName = "Bart",
+                    LastName = "Nogiets",
+                    IsActive = true,
+                    Team = new Team { Name = "OlaOle Vietske" }
+                }
+            };
+
+            // HARD CODED LIJST FILTEREN
+            var query = cyclists.AsQueryable();
+
+            // Zoeken op voornaam, achternaam of team
+            if (!string.IsNullOrWhiteSpace(searchString))
+            {
+                query = query.Where(c =>
+                    c.FirstName.Contains(searchString, StringComparison.OrdinalIgnoreCase) ||
+                    c.LastName.Contains(searchString, StringComparison.OrdinalIgnoreCase) ||
+                    (c.Team != null && c.Team.Name.Contains(searchString, StringComparison.OrdinalIgnoreCase)));
+            }
+
+            // Filter op IsActive
+            if (!string.IsNullOrWhiteSpace(activeFilter))
+            {
+                switch (activeFilter.ToLower())
+                {
+                    case "yes":
+                        query = query.Where(c => c.IsActive);
+                        break;
+
+                    case "no":
+                        query = query.Where(c => !c.IsActive);
+                        break;
+                }
+            }
+
+            ViewBag.SearchString = searchString;
+            ViewBag.ActiveFilter = activeFilter;
+
+            var filteredCyclists = query
+                .OrderBy(c => c.LastName)
+                .ThenBy(c => c.FirstName)
+                .ToList();
+
+            return View(filteredCyclists);
+
+            /*
+            // TOEKOMSTIGE DATABASECODE
             var query = _context.Cyclists
                 .Include(c => c.Team)
                 .AsQueryable();
@@ -43,15 +118,13 @@ namespace StageProject_RaceCore.Controllers
                 }
             }
 
-            ViewBag.SearchString = searchString;
-            ViewBag.ActiveFilter = activeFilter;
-
-            var cyclists = await query
+            var cyclistsFromDb = await query
                 .OrderBy(c => c.LastName)
                 .ThenBy(c => c.FirstName)
                 .ToListAsync();
 
-            return View(cyclists);
+            return View(cyclistsFromDb);
+            */
         }
     }
 }
