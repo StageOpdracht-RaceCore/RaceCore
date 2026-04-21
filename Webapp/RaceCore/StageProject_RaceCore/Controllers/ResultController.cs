12441440773b9040eaf8a2340be5d34d615afe84
DOUDOUH SAID <kibi1234dou@gmail.com>
2026-04-21 13:44:30 +0200

    pull

--- Webapp/RaceCore/StageProject_RaceCore/Controllers/ResultController.cs
+++ Webapp/RaceCore/StageProject_RaceCore/Controllers/ResultController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 
 namespace StageProject_RaceCore.Controllers
 {
+  public class ResultVM
+  {
+    public string StageName { get; set; } = "";
+    public string CyclistName { get; set; } = "";
+    public int points { get; set; }
+
+  }
     public class ResultController : Controller
     {
         public IActionResult Index()
         {
             return View();
         }
+
     }
+
+
 }
