34e35a9da984a84e87e6df8665f317ff40722aec
Jason Peleman <jason.peleman@student.ucll.be>
2026-04-22 13:48:24 +0200

    database updated, player fixed

--- Webapp/RaceCore/StageProject_RaceCore/Migrations/20260422113306_AddPlayerDraftFields.Designer.cs
+++ Webapp/RaceCore/StageProject_RaceCore/Migrations/20260422113306_AddPlayerDraftFields.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using StageProject_RaceCore.Models;
 
@@ -10,9 +11,11 @@ using StageProject_RaceCore.Models;
 namespace StageProject_RaceCore.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260422113306_AddPlayerDraftFields")]
+    partial class AddPlayerDraftFields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.6");
@@ -344,7 +347,8 @@ namespace StageProject_RaceCore.Migrations
 
                     b.HasKey("Id");
 
-                    b.HasIndex("Tag");
+                    b.HasIndex("Tag")
+                        .IsUnique();
 
                     b.ToTable("Teams");
                 });
--- Webapp/RaceCore/StageProject_RaceCore/Models/Player.cs
+++ Webapp/RaceCore/StageProject_RaceCore/Models/Player.cs
@@ -3,6 +3,15 @@
     public class Player
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public int PositionInDraft { get; set; }
+
+        public int TotalPoints { get; set; } = 0;
+
+        public ICollection<PlayerSelection> Selections { get; set; } = new List<PlayerSelection>();
+
+        public ICollection<DraftTurn> DraftTurns { get; set; } = new List<DraftTurn>();
     }
 }
