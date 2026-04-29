d9850de47c187e16c80641cc042adad2c12bee6a
Daniel St <webdeconline@gmail.com>
2026-04-29 15:56:34 +0200

    verwijderen sidebar test new game pagina

---
+++ Webapp/RaceCore/RaceCore/StageProject_RaceCore/Views/Shared/_CleanLayout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StageProject_RaceCore.Views.Shared
+{
+    public class _CleanLayoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
